120c4825af147f9ae3bd7af7d45c2d7a718e7a2c
zatrit <zatrit@gmail.com>
2025-04-04 20:41:24 +0300

    Simplified build.rs, attempt to fix line endings in Windows builds

--- build.rs
+++ build.rs
@@ -1,11 +1,11 @@
-use std::{env, fs, io::ErrorKind, path::PathBuf};
+use std::{env, error::Error, fs, io::ErrorKind, path::PathBuf};
 
 use alpacker::pack::TarZstPack;
 use alpacker_packer::{AssetsBuilder, PackBuilder, transform::OxipngTransform};
 
-fn main() {
-    let profile = env::var("PROFILE").unwrap(); // e.g. "release" or "debug"
-    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+fn main() -> Result<(), Box<dyn Error>> {
+    let profile = env::var("PROFILE")?; // e.g. "release" or "debug"
+    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?);
     let target_dir = env::var("CARGO_TARGET_DIR")
         .map(PathBuf::from)
         .unwrap_or_else(|_| manifest_dir.join("target"));
@@ -15,24 +15,20 @@ fn main() {
 
     match fs::remove_file(target_path.join("manifest.json")) {
         Err(error) if error.kind() == ErrorKind::NotFound => {}
-        result => result.unwrap(),
+        result => result?,
     };
     match fs::remove_file(target_path.join("content.tar.zst")) {
         Err(error) if error.kind() == ErrorKind::NotFound => {}
-        result => result.unwrap(),
+        result => result?,
     };
 
-    let pack = PackBuilder::new("content")
-        .unwrap()
-        .copy_from("assets/")
-        .unwrap()
-        .transform(&mut OxipngTransform::default())
-        .unwrap();
+    let pack = PackBuilder::new("content")?
+        .copy_from("assets/")?
+        .transform(&mut OxipngTransform::default())?;
 
-    AssetsBuilder::new(target_path, "./")
-        .unwrap()
-        .add_pack::<TarZstPack>("content", &pack)
-        .unwrap()
-        .write_manifest()
-        .unwrap();
+    AssetsBuilder::new(target_path, "./")?
+        .add_pack::<TarZstPack>("content", &pack)?
+        .write_manifest()?;
+
+    Ok(())
 }
